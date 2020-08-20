4ceaff28d81144c4b9b9cd460b5c40c5903f6f9e
vy3191 <venkyafhs@gmail.com>
2020-08-20 15:15:41 -0400

    Completed queue assignment using lists

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,25 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+from singly_linked_list import LinkedList
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        index = self.size
+        self.storage.insert(index, value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.size > 0:
+            first_index = 0
+            del_value = self.storage[first_index]
+            del self.storage[first_index]
+            self.size -= 1
+            return del_value
