f4600d56db012d6378605af81a6394fcbae72493
vy3191 <venkyafhs@gmail.com>
2020-08-20 14:52:34 -0400

    Implemented stacks using singly linked list

---
+++ stack/singly_linked_list.py
@@ -0,0 +1,118 @@
+class Node:
+  # An object for storing a single node in a linked list
+  def __init__(self, value, next_node=None):
+    self.value = value  # data stored in the node
+    self.next_node = next_node  # Reference to next node in the linked list
+
+  def get_value(self):
+    return self.value
+
+  def get_next_node(self):
+    return self.next_node
+
+  def set_next_node(self, new_next_node):
+    self.next_node = new_next_node
+
+  def __repr__(self):
+    return f"<Node value: {self.value}>"
+
+class LinkedList:
+  # A linear data structure that stores values in nodes
+  def __init__(self, head=None, tail=None):
+    self.head = head
+    self.tail = tail
+    self.length = 0
+  def is_list_empty(self):
+    return self.head == None
+  def find_size(self):
+    current = self.head   # O(1)
+    count = 0
+    while current:
+      count += 1
+      current = current.next_node
+    return count  
+
+  def add_to_head(self,value):
+    # if there is no node at all
+    new_node = Node(self,value)
+    if self.head == None:
+      self.head = new_node
+      self.tail = new_node
+      self.head.next_node = None
+    else:
+      new_node.set_next_node(self.head)  
+      self.head = new_node
+    self.length +=1
+
+  def add_to_tail(self,value):
+    new_node = Node(value)
+    if self.head is None:    #  O(1)
+      self.head = new_node
+      self.tail = new_node
+    else:
+      self.tail.set_next_node(new_node)  #  O(1)
+      self.tail = new_node      
+    self.length += 1  
+
+  def remove_head(self):
+    # say if there are no node at all in the list
+    deleted_value = None
+    if self.head is None:
+      return None
+    elif self.head == self.tail:
+      deleted_value = self.head.get_value()
+      self.head = None
+      self.tail = None
+      self.length -= 1
+    # if there are more than one node
+    else:
+      deleted_value = self.head.get_value()
+      self.head = self.head.get_next_node()
+      self.length -= 1
+    return deleted_value  
+
+  def remove_tail(self):
+    # if there are no nodes to remove
+    if self.tail is None:
+      return None
+    elif self.head == self.tail:
+      deleted_value = self.tail.value
+      print(f"line81{deleted_value}")
+      self.head = None
+      self.tail = None
+      self.length -= 1
+      return deleted_value
+    else:
+      current = self.head
+      while current.next_node is not self.tail:
+        print(current.value)
+        current = current.next_node
+      print(f"value before tail is: {current.value}")  
+      deleted_value = self.tail.value
+      self.tail = current
+      current.next_node = None  
+      self.length -=1   
+
+    return deleted_value
+
+  def __repr__(self):
+    current = self.head
+    nodes = []
+    while current:
+      print(current.value)
+      if current is self.head:
+        nodes.append(f"[Head: {current.value}]") 
+        current = current.next_node
+      elif current.next_node is None:
+        nodes.append(f"[Tail: {current.value}]")
+        current = current.next_node 
+      else:
+        nodes.append(f"[{current.value}]")  
+        current = current.next_node
+    return '-->'.join(nodes)    
+      
+
+
+
+
+
--- stack/stack.py
+++ stack/stack.py
@@ -10,23 +10,24 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+from singly_linked_list import LinkedList
 class Stack:
     def __init__(self):
         self.size = 0
         # self.storage = ?
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
 
     def push(self, value):
-        index = len(self.storage)
-        self.storage.insert(index, value)
-        self.size += 1
+        # index = len(self.storage)
+        # self.storage.insert(index, value)
+        if self.size > 0:
+            self.storage.add_to_tail(value)
+            self.size += 1
 
     def pop(self):
-        if len(self.storage) > 0:
-            self.size -= 1
-            return self.storage.pop()
-        else: 
-            return None    
+        self.storage.remove_tail()
+        self.size -= 1
+          
